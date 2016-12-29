29c288f1dad1d66c0feec97a1150054737c7966d
ihrimech <hrimechismail@gmail.com>
2016-12-29 07:55:04 +0100

    settings for front page set

--- app/frontpage/accueil.component.ts
+++ app/frontpage/accueil.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { configDisplay } from './../config/display';
 @Component({
     selector: 'accueil',
     templateUrl: 'app/frontpage/accueil.component.html'
 })
 export class AccueilComponent implements OnInit {
     constructor() { }
+    
+    public displayOptions = configDisplay;
 
     ngOnInit() { }
 }
